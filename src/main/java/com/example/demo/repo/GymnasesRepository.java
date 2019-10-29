0484ccb6b3a4430fd1107c468fefaf70e363f77f
tetedestup <45398660+tetedestup@users.noreply.github.com>
2019-10-29 15:57:10 +0100

    v5

--- src/main/java/com/example/demo/repo/GymnasesRepository.java
+++ src/main/java/com/example/demo/repo/GymnasesRepository.java
@@ -1,35 +1,23 @@
 package com.example.demo.repo;
 
-
-
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.demo.modul.Gymnases;
 
-public interface GymnasesRepository extends MongoRepository<Gymnases, String>{
-	
-	
-	public List <Gymnases> findByIdGymnase(Long IdGymnase);
-	
-	public List <Gymnases> findByNomGymnase(String NomGymnase);
-	
-	public List <Gymnases> findByAdresse(String Adresse);
-	
-	public List <Gymnases> findByVille(String Ville);
-	
-	public List <Gymnases> findBySurface(Double Surface);
-	
-	
+public interface GymnasesRepository extends MongoRepository<Gymnases, String> {
 
+	public List<Gymnases> findByIdGymnase(Long IdGymnase);
 
+	public List<Gymnases> findByNomGymnase(String NomGymnase);
 
+	public List<Gymnases> findByAdresse(String Adresse);
 
-	
-	
-	
-	
+	public List<Gymnases> findByVille(String Ville);
+
+	public List<Gymnases> findBySurface(Double Surface);
+
 //	@Id
 //	@Field(value = "IdGymnase")
 //    private Long IdGymnase;
@@ -48,7 +36,5 @@ public interface GymnasesRepository extends MongoRepository<Gymnases, String>{
 //	
 //	@Field(value = "Seances")
 //	private List<Seances> seances;
-	
-	
 
 }
